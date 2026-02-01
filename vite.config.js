8c51c42e23eba8d1fe9ae4e4d3417220744d4861
Mamun2k22 <mamun180037@gmail.com>
2026-02-01 20:47:03 +0600

    Fix package.json and prepare for vercel

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Vercel এর জন্য optimize
-  build: {
-    outDir: 'dist',
-    sourcemap: false,
-    chunkSizeWarningLimit: 1600
-  },
-  server: {
-    port: 3072
-  }
 })
